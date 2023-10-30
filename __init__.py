5d1b3f2093a40640bf49f08e6ff97f8401c17609
Shea Kennedy <sheamkennedy@gmail.com>
2023-10-30 11:48:32 -0500

    Update __init__.py
    Changed bl_info to reflect project branch.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,18 @@
 # Copyright 2022 by Hextant Studios. https://HextantStudios.com
 # This work is licensed under GNU General Public License Version 3.
 # License: https://download.blender.org/release/GPL3-license.txt
+# Modified by Shea Kennedy, Oct. 30 2023.
+# Modifications made:
+# --- Changed bl_info to reflect project branch.
 
 bl_info = {
     "name": "Batch Exporter",
-    "author": "Hextant Studios",
+    "author": "Doffu",
     "version": (1, 0, 0),
     "blender": (3, 2, 0),
     "location": "Scene > Batch Exporter",
     "description": "A batch exporter which currently supports glTF and FBX formats.",
-    "doc_url": "https://github.com/hextantstudios/hextant_batch_exporter",
+    "doc_url": "https://github.com/sheamkennedy/blender_batch_exporter",
     "category": "Import-Export",
 }
 
